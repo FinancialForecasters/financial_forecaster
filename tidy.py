f7ccd12bc2a67f34675e66854af7b25e8f9abb15
AJ Martinez <96085262+AJMartinezLlanes@users.noreply.github.com>
2022-06-02 10:16:38 -0500

    adding miner features

--- tidy.py
+++ tidy.py
@@ -4,6 +4,7 @@ pull the latest 1000 candlestick entries from the binance api
 '''
 
 from imports import *
+from talib import atr
 
 def csv_btcusd():
 	if os.path.exists('BTC-USD.csv'):
@@ -204,4 +205,37 @@ def prepare_df(df, column, extra_words = [], exclude_words = []):
     df['stemmed_word_count'] = df.stemmed.str.split().str.len()
     df['lemmatized_word_count'] = df.lemmatized.str.split().str.len()
 
+    return df
+
+## Miner features aka AJ Features
+
+def add_csv(df, filename):
+    '''
+    This fuction will add a csv data to the main dataframe
+    '''
+    # read the CSV file and assign a variable
+    filename_df = pd.read_csv(f'~/codeup-data-science/financial_forecaster/project_csvs/{filename}.csv')
+    # change dtype of timestamp into pandas date
+    filename_df.Timestamp = pd.to_datetime(filename_df.Timestamp).dt.date
+    # reset index to datetime
+    filename_df = filename_df.set_index('Timestamp').sort_index()
+    # reset index to datetime for dataframe
+    df.index = pd.to_datetime(df.index)
+    # add the CSV_dataframe to given dataframe
+    df[filename] = filename_df
+    # fill the nulls
+    df.fillna(method='ffill', inplace=True)
+    # retunrs a dataframe
+    return df
+
+def add_miner_features(df):  
+    '''
+    This functino will add all the miner CSVs to a main dataframe
+    '''
+    # add all the CSV files to a variable
+    csv_filenames = ['avg-fees-per-transaction', 'cost-per-transaction-percent', 'cost-per-transaction', 'difficulty', 'hash-rate', 'miners-revenue', 'transaction-fees-to-miners']
+    # loop each CSV into the dataframe using add_cvs function
+    for filename in csv_filenames:
+        add_csv(df, filename)
+    # return df
     return df
