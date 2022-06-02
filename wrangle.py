b0981154a9181fa49f1bdf354553fc05c1f590d4
watermanbrandyn <watermanbrandyn@gmail.com>
2022-06-02 10:29:57 -0500

    wrangle

--- wrangle.py
+++ wrangle.py
@@ -6,15 +6,15 @@ def wrangle_df():
     Acquires initial dataframe from yfinance and then adds engineered features.
     '''
     # Our intial dataframe
-    df = explore_df()
+    df = tidy.explore_df()
     # Adding macd engineered features
-    df = macd_df(df)
+    df = tidy.macd_df(df)
     # Adding time engineered features
-    df = time_features(df)
+    df = tidy.time_features(df)
     # Adding atr feature
-    df = add_ATR_feature(df)
+    df = tidy.add_ATR_feature(df)
     # Adding miner features
-    df = add_miner_features(df)
+    df = tidy.add_miner_features(df)
 
     # Return df
     return df
