06e05ff630cb624077fd678b3b6e851f7f1f2229
Jesse Marder <jesse.d.marder@gmail.com>
2022-06-10 11:08:07 -0500

    Update with additional functions to support

--- explore.py
+++ explore.py
@@ -1,4 +1,36 @@
 from imports import *
+from wrangle import *
+import matplotlib.ticker as mtick
+from matplotlib import gridspec
+
+def plot_close_price_and_returns(price_df):
+    """Plots chart of closing price with percentage returns below. Used for final presentation and report """
+    
+    # Get price data
+    df = price_df.copy()
+    
+    fig = plt.figure(figsize = (16,8))
+    gs = gridspec.GridSpec(2, 1, height_ratios = [2,1])
+    ax0 = plt.subplot(gs[0])
+    ax2 = plt.subplot(gs[0])
+    # ax2 = ax0.twinx()
+    ax0.plot(df.close)
+    # ax2.plot(spy['Adj Close'], 'g', alpha = 0.2)
+    ax0.set_ylabel('Close Price ($)', fontsize = 20)
+
+
+    plt.setp(ax0.get_xticklabels(), visible = False)
+
+    ax1 = plt.subplot(gs[1], sharex = ax0)
+    ax1.plot(df.fwd_pct_chg*100)
+    ax1.set_ylabel('Percent Returns', fontsize = 18)
+    ax1.axhline(y=10, alpha = 0.3)
+    ax1.axhline(y=-10, alpha = 0.3)
+    ax1.yaxis.set_major_formatter(mtick.PercentFormatter())
+    ax0.set_title('Bitcoin Close Price and Percentage Returns', fontsize = 20)
+    ax0.set_xlim(left = pd.Timestamp('2018-01-01'), right = pd.Timestamp('2022-05-30'))
+
+    plt.subplots_adjust(hspace = 0)
 
 def perform_target_return_analysis(df, target = 'fwd_log_ret'):
     """Plots a histogram of returns"""
@@ -68,3 +100,36 @@ def plot_consolidated_atr_analysis(df):
     plt.ylabel('Average Return')
     plt.axhline(df.fwd_log_ret.mean(), color = 'r')
     plt.annotate('Overall Mean', (5, 0.0017))
+    
+def plot_sentiment_and_close(price_df, csv_file = './csv/weekly_consolidated_later_sentiment.csv'):
+    """Plot aggregated Twitter sentiment with close price going back to 2019 """
+    
+    # Read in aggregated tweet csv 
+    lt = pd.read_csv(csv_file, index_col = 'time', parse_dates=True)
+    
+    # Read in price data
+    df = price_df.copy()
+
+    fig, ax = plt.subplots(figsize=(18,8))
+    
+    # Plot sentiment
+    sentiment = ax.plot(lt, color = 'salmon', label = 'sentiment')
+    ax.set_xlabel("Date", fontsize = 24)
+    ax.set_ylabel("Average Twitter Sentiment", fontsize = 24)
+    ax.tick_params(axis = 'both', which = 'major', labelsize = 18)
+
+    # Plot close price data
+    ax2 = ax.twinx()
+    close_price = ax2.plot(df.loc[df.index>'2019-09-19'].close, color = 'mediumseagreen', label = 'close price')
+    ax2.set_ylabel("Close Price ($)", fontsize = 24)
+    
+    # Clean up the legend
+    lns = sentiment + close_price
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs, loc = 2, fontsize = 24)
+    
+    plt.yticks(fontsize=18)
+    plt.tight_layout()
+    plt.title('Twitter Sentiment and Bitcoin Close Price', fontsize = 24)
+    plt.show()
+
